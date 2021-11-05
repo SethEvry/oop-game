fe3fa473e3ac268b5c073626cbeaf8a2fecb7441
SethEvry <92412142+SethEvry@users.noreply.github.com>
2021-11-05 15:49:46 -0400

    create classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,42 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor(phrases){
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
+
+    /**
+     * initializes the game
+     */
+    startGame() {}
+
+    /**
+     * retrieves a random phrase from the phrases array
+     * 
+     * @return {Object} - a Phrase
+     */
+    getRandomPhrase() {}
+
+    /**
+     * controls game logic from DOM intercation
+     */
+    handleInteraction() {}
+    /**
+     * takes a life from the scoreboard
+     */
+    removeLife() {}
+
+    /**
+     * checks for win conditions
+     */
+    checkForWin() {}
+
+    /**
+     * ends the game and display start screen
+     */
+    gameOver() {}
+    
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase{
+    constructor(phrase){
+        this.phrase = phrase.tolowerCase();
+
+    }
+    /**
+     * Adds letter placeholders to the display
+     * 
+     */
+    addPhraseToDisplay(){}
+
+    /**
+     * Checks to see if the letter selected matches
+     */
+    checkLetter(){}
+
+    /**
+     * reveals letter(s) on the board that match the selection
+     */
+    showMatchedLetter(){}
+
+
+}
